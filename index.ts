f2780ef2e50076cd1da574c125aa01258dba86e2
mrszeeshan <139902365+mrszeeshan@users.noreply.github.com>
2023-07-23 18:24:38 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,8 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+let PersonName:string = "Nareen"
+let massage: string ="Hello ${PersonName}, hope you are well these days."
+console.log(massage)
+
+
